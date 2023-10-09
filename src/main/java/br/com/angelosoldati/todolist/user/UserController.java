eb1d22e368adc2228228abc575f30c9d81be53f5
angelolavorato <angelolavorato@hotmail.com>
2023-10-09 19:05:34 -0300

    configurando o userModel

--- src/main/java/br/com/angelosoldati/todolist/user/UserController.java
+++ src/main/java/br/com/angelosoldati/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.angelosoldati.todolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-/*
- * String (Texto)
- * Interger (int)
- * Double 
- * Float
- * char
- * Date
- * void
- */
+  /*
+   * String (Texto)
+   * Interger (int)
+   * Double
+   * Float
+   * char
+   * Date
+   * void
+   */
+  @PostMapping("/")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.name);
 
-  public void create(){
-    
   }
 
 }
--- src/main/java/br/com/angelosoldati/todolist/user/UserModel.java
+++ src/main/java/br/com/angelosoldati/todolist/user/UserModel.java
@@ -1,5 +1,8 @@
 package br.com.angelosoldati.todolist.user;
 
 public class UserModel {
-  
+  public String username;
+  public String name;
+  public String password;
+
 }
