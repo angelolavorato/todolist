789f9317130efe2f140d52399dbd71649898bd34
angelolavorato <angelolavorato@hotmail.com>
2023-10-09 18:40:51 -0300

    confirgurando o controller para fazer a primeira run

--- src/main/java/br/com/angelosoldati/todolist/controller/MinhaPrimeiraController.java
+++ src/main/java/br/com/angelosoldati/todolist/controller/MinhaPrimeiraController.java
@@ -1,7 +1,28 @@
 package br.com.angelosoldati.todolist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/primeiraRota")
+// http://localhost:8080/------------------------------------------------
 public class MinhaPrimeiraController {
-  
+  /**
+   * Métdos de acesso do HTTP
+   * GET - Buscar uma informação
+   * POST- Adicionar um dado/informação
+   * PUT- Alterar um dado/informação
+   * DELETE - Remover um dado/informação
+   * PATCH - Alterar somente uma parte da informação/dado
+   * 
+   */
+
+  // Método (funcionalidade ) de uma classe
+  @GetMapping("/")
+  public String primeiraMensagem() {
+    return "Funcionou";
+
+  }
 }
