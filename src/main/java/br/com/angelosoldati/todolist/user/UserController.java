77d1c819529b0463c4b5f6624fc8c5e29d7513fb
angelolavorato <angelolavorato@hotmail.com>
2023-10-09 18:59:44 -0300

    criando o user model

--- src/main/java/br/com/angelosoldati/todolist/user/UserController.java
+++ src/main/java/br/com/angelosoldati/todolist/user/UserController.java
@@ -1,5 +1,31 @@
 package br.com.angelosoldati.todolist.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * Public
+ * Private
+ * Protected
+ */
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-  
+
+/*
+ * String (Texto)
+ * Interger (int)
+ * Double 
+ * Float
+ * char
+ * Date
+ * void
+ */
+
+  public void create(){
+    
+  }
+
 }
