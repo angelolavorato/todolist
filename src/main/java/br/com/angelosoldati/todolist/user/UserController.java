8efe0c60f02ddfaf692c21fa2f8a98ed6d42ca78
angelolavorato <angelolavorato@hotmail.com>
2023-10-11 21:41:48 -0300

    adicionando o IURepository e configurando o user controller

--- src/main/java/br/com/angelosoldati/todolist/user/UserController.java
+++ src/main/java/br/com/angelosoldati/todolist/user/UserController.java
@@ -1,9 +1,12 @@
 package br.com.angelosoldati.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.var;
 
 /**
  * Modificador
@@ -16,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   /*
    * String (Texto)
    * Interger (int)
@@ -26,9 +32,9 @@ public class UserController {
    * void
    */
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
-
+  public UserModel create(@RequestBody UserModel userModel) {
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 
 }
