e76b59bb47cada4a31e8a99a1294814aa68ca66e
angelolavorato <angelolavorato@hotmail.com>
2023-10-12 20:53:26 -0300

    modelando a tabela\

--- src/main/java/br/com/angelosoldati/todolist/task/TaskModel.java
+++ src/main/java/br/com/angelosoldati/todolist/task/TaskModel.java
@@ -1,23 +1,46 @@
 package br.com.angelosoldati.todolist.task;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/*
+ * O que a tarefa deve ter:
+ * ID
+ * Usuário (ID_Usuário)
+ * Descrição
+ * Título
+ * Data de início
+ * Data de término
+ * Prioridade
+ */
+
+@Data
+@Entity(name = "tb_tasks")
 public class TaskModel {
 
-  /*
-   * O que a tarefa deve ter:
-   * ID
-   * Usuário (ID_Usuário)
-   * Descrição
-   * Título
-   * Data de início
-   * Data de término
-   * Prioridade
-   */
-
+  @Id
+  @GeneratedValue(generator = "UUID")
   private UUID id;
   private String description;
+
+  @Column(length = 50)
   private String title;
   private LocalDateTime startAt;
   private LocalDateTime endAt;
+  private String priority;
+
+  private UUID idUser;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
 }
