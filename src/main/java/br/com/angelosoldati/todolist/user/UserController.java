ac88f1018ff0b20bf868808a462abb8fb94c3987
angelolavorato <angelolavorato@hotmail.com>
2023-10-11 22:13:21 -0300

    melhorando a funcionalidade de usuário existente ou não

--- src/main/java/br/com/angelosoldati/todolist/user/UserController.java
+++ src/main/java/br/com/angelosoldati/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.angelosoldati.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,17 +34,19 @@ public class UserController {
    * void
    */
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
 
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
-      System.out.println("Usuário Já existe	");
-      return null;
+
+      // Mensagem de erro
+      // Status Code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já existe");
     }
 
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 
 }
