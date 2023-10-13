f5436b931b798ce905c6e0398fa89cbd4964c3df
angelolavorato <angelolavorato@hotmail.com>
2023-10-12 22:32:40 -0300

    modificando o filtertaskauth, a aplicação parou de subir.. NICE!

--- src/main/java/br/com/angelosoldati/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/angelosoldati/todolist/filter/FilterTaskAuth.java
@@ -3,19 +3,26 @@ package br.com.angelosoldati.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.fasterxml.jackson.databind.JsonSerializable.Base;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.angelosoldati.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.experimental.var;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -37,14 +44,26 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     System.out.println(username);
     System.out.println(password);
 
-    
     // Validar usuário
+    var user = this.userRepository.findByUsername(username);
+    if (user == null) {
+    } else {
 
-    // Validar senha
+      // Validar senha
+      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+      if (passwordVerify.verified) {
 
-    // segue
+        filterChain.doFilter(request, response);
 
-    filterChain.doFilter(request, response);
+      } else {
+
+        response.sendError(401);
+
+      }
+      // segue
+
+    }
+
   }
 
 }
