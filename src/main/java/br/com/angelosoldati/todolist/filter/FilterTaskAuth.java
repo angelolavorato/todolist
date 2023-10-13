f0cbeb744d937c9c2137c47e32bd51f0ce79ca83
angelolavorato <angelolavorato@hotmail.com>
2023-10-12 22:07:52 -0300

    fazendo o FilterTaskAuth desencriptar as informações, cortar e separar as infos de usuário e senha

--- src/main/java/br/com/angelosoldati/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/angelosoldati/todolist/filter/FilterTaskAuth.java
@@ -1,26 +1,50 @@
 package br.com.angelosoldati.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
+import com.fasterxml.jackson.databind.JsonSerializable.Base;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-    // Executar alguma ação
+    // Pegar a autenticação (Usuario e senha)
+    var authorization = request.getHeader("Authorization");
 
-    System.out.println("chegou no filtro");
-    chain.doFilter(request, response);
+    var authEncoded = authorization.substring("Basic".length()).trim();
+
+    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+    var authString = new String(authDecode);
+
+    String[] credentials = authString.split(":");
+    String username = credentials[0];
+    String password = credentials[1];
+    System.out.println("Authorization");
+    System.out.println(username);
+    System.out.println(password);
+
+    
+    // Validar usuário
+
+    // Validar senha
+
+    // segue
+
+    filterChain.doFilter(request, response);
   }
 
 }
